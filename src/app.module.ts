7f99d58a285e997cf9fd75b5d212de1616a89b70
Rivaldo12 <rivaldo2005@gmail.com>
2024-12-01 13:42:16 -0300

    feat(measures): add measures resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './common/prisma.service';
+import { MeasuresModule } from './measures/measures.module';
 
 @Module({
-  imports: [],
+  imports: [MeasuresModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
